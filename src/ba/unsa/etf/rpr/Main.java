5db4971331f2d2b1647909337f5c6f82230ddec0
student <skurtovic1@etf.unsa.ba>
2021-10-21 17:01:58 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Unesite n");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+    }
+}
