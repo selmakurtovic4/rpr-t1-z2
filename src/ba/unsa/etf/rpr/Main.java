bc72d40052d68f83051dd46f550bd2301bc22f85
student <skurtovic1@etf.unsa.ba>
2021-10-21 17:20:47 +0200

    gotovo

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -8,5 +8,20 @@ public class Main {
         System.out.println("Unesite n");
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
+        for (int i = 1; i < n; i++) {
+            sumaCifara(i);
+           if (i % sumaCifara(i) == 0 )
+                System.out.println(i);
+        }
+    }
+    static int sumaCifara(int n){
+        int suma=0;
+        do{
+           suma+=n%10;
+           n=n/10;
+
+        }
+        while(n!=0);
+        return suma;
     }
 }
